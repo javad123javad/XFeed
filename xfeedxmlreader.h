13cce825b3aea66de08404a07a298af33b3c1c60
Javad Rahimipetroudi <javad321javad@gmail.com>
2025-03-04 12:19:04 +0100

    Add Getter/Setter

--- xfeedxmlreader.h
+++ xfeedxmlreader.h
@@ -3,6 +3,7 @@
 
 #include "xfeedreader.h"
 #include "xfeedch.h"
+#include "xfeedfolder.h"
 #include <memory.h>
 class XFeedXMLReader : public XFeedReader
 {
@@ -10,9 +11,14 @@ public:
     XFeedXMLReader();
     virtual ~XFeedXMLReader() = default;
     void addChannel(ChannelInfo& chInfo) override;
+    void addFolder(std::shared_ptr<XFeedFolder> folder);
+
     void readFeed()  override;
 
+    const QVector<std::shared_ptr<XFeedFolder> >& folderList() const;
+
 private:
+    QVector<std::shared_ptr<XFeedFolder>> folderList_;
     QVector< std::shared_ptr<XFeedCh>> channelList_;
 };
 
