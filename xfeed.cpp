a07aeca1b30aa63a7d650378362cd733977d5e49
Javad Rahimipetroudi <javad321javad@gmail.com>
2025-03-04 12:16:30 +0100

    Add triggers for Sub Widgets

--- xfeed.cpp
+++ xfeed.cpp
@@ -1,5 +1,7 @@
 #include "xfeed.h"
 #include "./ui_xfeed.h"
+#include "addchannel.h"
+#include "addfolder.h"
 
 XFeed::XFeed(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +14,30 @@ XFeed::~XFeed()
 {
     delete ui;
 }
+
+
+
+void XFeed::on_actionAdd_Channel_triggered()
+{
+
+    AddChannel addCh(feedFolders_, this);
+    addCh.setFolderList(feedFolders_);
+    if(addCh.exec())
+    {
+        ChannelInfo chInfo = addCh.getChInfo();
+        xreader.addChannel(chInfo);
+    }
+
+}
+
+
+void XFeed::on_actionAdd_Folder_triggered()
+{
+    AddFolder addFolderDlg_(feedFolders_, this);
+    if(addFolderDlg_.exec())
+    {
+        feedFolders_.push_back(std::make_shared<XFeedFolder>(addFolderDlg_.folderName()));
+    }
+
+}
+
--- xfeed.h
+++ xfeed.h
@@ -2,12 +2,16 @@
 #define XFEED_H
 
 #include <QMainWindow>
+#include <memory>
+#include "xfeedxmlreader.h"
+#include "xfeedfolder.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class XFeed;
 }
 QT_END_NAMESPACE
+
 
 class XFeed : public QMainWindow
 {
@@ -19,5 +23,11 @@ public:
 
 private:
     Ui::XFeed *ui;
+    XFeedXMLReader xreader;
+    folder_list_t feedFolders_;
+
+private slots:
+    void on_actionAdd_Channel_triggered();
+    void on_actionAdd_Folder_triggered();
 };
 #endif // XFEED_H
