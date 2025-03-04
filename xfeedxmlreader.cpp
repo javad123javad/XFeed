9b4e3837060e8a58a73973b23481cbf8d4d94a3a
Javad Rahimipetroudi <javad321javad@gmail.com>
2025-03-04 12:18:10 +0100

    add Getter/Setter for folders

--- xfeedxmlreader.cpp
+++ xfeedxmlreader.cpp
@@ -8,7 +8,22 @@ void XFeedXMLReader::addChannel(ChannelInfo& chInfo)
     channelList_.push_back(std::make_shared<XFeedCh>(chInfo));
 }
 
+void XFeedXMLReader::addFolder(std::shared_ptr<XFeedFolder> folder)
+{
+
+    if(!folder)
+        throw std::invalid_argument("folder parameter can not be empty");
+
+    folderList_.append(folder);
+}
+
+
 void XFeedXMLReader::readFeed()
 {
 
+}
+
+const QVector<std::shared_ptr<XFeedFolder> >& XFeedXMLReader::folderList() const
+{
+    return folderList_;
 }
