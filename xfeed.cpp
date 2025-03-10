2a960d2a517f1bf79727475bbb535030e5c0fb9d
Javad Rahimipetroudi <javad321javad@gmail.com>
2025-03-10 11:44:21 +0100

    update controller base on new MVC architecture
    This commit applys required change to able the controller part to use
    Model class rather than using the internal variables directly.

--- xfeed.cpp
+++ xfeed.cpp
@@ -2,12 +2,21 @@
 #include "./ui_xfeed.h"
 #include "addchannel.h"
 #include "addfolder.h"
-
+#include "xjsonadapter.h"
 XFeed::XFeed(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::XFeed)
 {
     ui->setupUi(this);
+    try {
+
+        // XJSonAdapter jsonAdapter_();
+        model_ = xmodel_.getModelFromData("/home/javad/workspace/qt_workspace/XMLFeedReader/db.json");
+
+        ui->xtree->setModel(model_.get());
+    } catch (std::logic_error e) {
+        qDebug()<<e.what();
+    }
 }
 
 XFeed::~XFeed()
@@ -33,11 +42,23 @@ void XFeed::on_actionAdd_Channel_triggered()
 
 void XFeed::on_actionAdd_Folder_triggered()
 {
+    // Get data from view
     AddFolder addFolderDlg_(feedFolders_, this);
     if(addFolderDlg_.exec())
     {
         feedFolders_.push_back(std::make_shared<XFeedFolder>(addFolderDlg_.folderName()));
     }
+    // Send data to the Model for further processing
+    //XFeedModel->addFolder(FolderName);
+    xmodel_.addFolder(addFolderDlg_.folderName());
 
+}
+
+
+void XFeed::on_xtree_clicked(const QModelIndex &index)
+{
+    auto item = model_->itemFromIndex(index);
+
+    qDebug()<<item->data(Qt::UserRole).toString();
 }
 
